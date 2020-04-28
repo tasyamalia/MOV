fbdc06893ddab227745d1c4c2b00a2c63e64049b
Tasya Amalia Salsabila <tasya.salsabila@visionet.co.id>
2020-04-28 10:42:06 +0700

    add onBoarding Screen

---
+++ app/src/main/java/com/tasya/mov/OnBoarding/OnBoardingOneActivity.kt
@@ -0,0 +1,13 @@
+package com.tasya.mov.OnBoarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tasya.mov.R
+
+class OnBoardingOneActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding_one)
+    }
+}
--- app/src/main/java/com/tasya/mov/OnBoarding/OnBoardingThree.kt
+++ app/src/main/java/com/tasya/mov/OnBoarding/OnBoardingThree.kt
@@ -1,12 +1,13 @@
-package com.tasya.mov
+package com.tasya.mov.OnBoarding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tasya.mov.R
 
-class MainActivity : AppCompatActivity() {
+class OnBoardingThree : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_on_boarding_three)
     }
 }
